2e6fe4269d1f5a0beba383b3b65f45b2622bee8f
Jeremy Jalal <jalal.jeremy@gmail.com>
2019-05-04 19:55:40 +0200

    Change prefix to easiris.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'easiris-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
