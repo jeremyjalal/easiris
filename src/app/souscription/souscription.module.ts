123f27e711a8d5f8729300fccdfbd7d3ee1596a0
Jeremy Jalal <jalal.jeremy@gmail.com>
2019-05-05 11:48:54 +0200

    Add souscription module

---
+++ src/app/souscription/souscription.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SouscriptionRoutingModule } from './souscription-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SouscriptionRoutingModule
+  ]
+})
+export class SouscriptionModule { }
